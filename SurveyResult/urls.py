31c7cd89bad931716ce5f5f0c0da2734438df1ce
Ishan <manandharishan6@gmail.com>
2022-07-04 15:41:04 +0545

    addedQuestionTextInsteadOfVraibleName

--- SurveyResult/urls.py
+++ SurveyResult/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import Home
+from .views import Home,Response
 
 app_name ='SurveyResult'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('ResponseList/',Response.as_view(),name='responselist')
 ]
--- SurveyResult/views.py
+++ SurveyResult/views.py
@@ -1,13 +1,22 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render
 from django.views import View
 import pandas as pd
 import ssaw
 from ssaw import InterviewsApi, QuestionnairesApi
+from ssaw.models import Group, QuestionnaireDocument
+from ssaw.utils import get_properties
 import json
+import numpy as np
+import warnings
 
 # Create your views here.
 
 df = pd.DataFrame()
+variablename=''
+question_df = pd.DataFrame()
+
+warnings.filterwarnings('ignore')
 
 class Home(View):
     def get(self,request):
@@ -33,34 +42,122 @@ class Home(View):
             global df0
             df0 =pd.merge(df0,globals()[f"df{i}"],how='outer')
         # print(df0)
-        present_columns = df0.columns.to_list()
         json_records = df0.reset_index().to_json(orient ='records')
         data = []
         data = json.loads(json_records)
-        for q in ssaw.QuestionnairesApi(client).get_list(questionnaire_id='c1a0eb87a5384aacba9b2c3fdf9ea19f'):
-            survey_title = q.title
-        context = {'data': data,'present_columns':present_columns,'survey_title':survey_title}
         global df
         df = df0.copy()
-        # question_variables = []
-        # df = df0.assign(Reason=df0['Reason'].str.split(',')).explode('Reason')
-        # print(df['Reason'].value_counts())
-        # for i in present_columns:
-        #     df = df0.assign(i=df0[i].str.split(',')).explode(i)
-        #     print(df[i].value_counts())
+        present_columns = df0.columns.to_list()
+        q_doc =  QuestionnairesApi(client).document(id = "c1a0eb87a5384aacba9b2c3fdf9ea19f",version = 2)
+        Group.update_forward_refs()
+
+        q = QuestionnaireDocument.parse_obj(q_doc)
+
+        question_txt = pd.DataFrame(columns=['variable_name','question_text'])
+
+        flat_list = get_properties(q)
+        for i in present_columns:
+            question_txt.loc[len(question_txt.index)] = [i,flat_list[i].question_text]
+
+        global question_df
+        question_df = question_txt.copy()
+
+        df0 = df0.rename(columns=question_txt.set_index('variable_name')['question_text'])
+
+        for q in ssaw.QuestionnairesApi(client).get_list(questionnaire_id='c1a0eb87a5384aacba9b2c3fdf9ea19f'):
+            survey_title = q.title
+        present_columns1 = df0.columns.to_list()
+        column_list = zip(present_columns,present_columns1)
+        context = {'data': data,'column_list':column_list,'survey_title':survey_title,'present_columns':present_columns,'present_columns1':present_columns1}
         return render(request,'SurveyResult/home.html',context)
 
+
     def post(self,request):
-        variablename = request.POST.get('variablename')
-        print(variablename)
+        variable = request.POST.get('variablename')
+        global variablename
+        variablename = variable
+        #print(variablename)
         global df
-        res = df.groupby(variablename).count().reset_index()
-        res = res.iloc[:,:2]
-        result = res.rename(columns={res.columns[1]:'Count'})
-        present_columns = result.columns.to_list()
-        json_records = result.reset_index().to_json(orient ='records')
-        data = []
-        data = json.loads(json_records)
-        context = {'data': data,'present_columns':present_columns}
-        return render(request,'SurveyResult/questionresult.html',context)
+        global question_df
+        client = ssaw.Client('https://cssapp.dishhome.com.np', 'DH_Datateam', 'Balen12345*#')
+        q_doc =  QuestionnairesApi(client).document(id = "c1a0eb87a5384aacba9b2c3fdf9ea19f",version = 2)
+        Group.update_forward_refs()
+        q = QuestionnaireDocument.parse_obj(q_doc)
+        flat_list = get_properties(q)
+        if flat_list[variablename].obj_type == 'MultyOptionsQuestion':
+            choice_list = []
+            for i in flat_list[variablename].answers:
+                choice_list.append(i.answer_text)
+            res = pd.DataFrame(columns=[variablename,'Count'])
+            for i in choice_list:
+                count = 0
+                for j in df[variablename]:
+                    if j is not None:
+                        if j.find(i) !=-1:
+                            count = count+1
+                res.loc[len(res.index)] = [i,count]
+            present_columns = res.columns.to_list()
+            print(res)
+            print(choice_list)
+            json_records = res.reset_index().to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            result = res.rename(columns=question_df.set_index('variable_name')['question_text'])
+            present_columns1 = result.columns.to_list()
+            context = {'data': data,'present_columns':present_columns,'variablename':variablename,'present_columns1':present_columns1}
+            return render(request,'SurveyResult/questionresult.html',context)
+        else:
+            res = df.groupby(variablename).count().reset_index()
+            res = res.iloc[:,:2]
+            result = res.rename(columns={res.columns[1]:'Count'})
+            present_columns = result.columns.to_list()
+            json_records = result.reset_index().to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            result = result.rename(columns=question_df.set_index('variable_name')['question_text'])
+            present_columns1 = result.columns.to_list()
+            context = {'data': data,'present_columns':present_columns,'variablename':variablename,'present_columns1':present_columns1}
+            return render(request,'SurveyResult/questionresult.html',context)
+
+
+
+
+class Response(View):
+    def get(self,request):
+        option_value = request.GET.get("name")
+        global variablename
+        global df
+        global question_df
+        client = ssaw.Client('https://cssapp.dishhome.com.np', 'DH_Datateam', 'Balen12345*#')
+        q_doc =  QuestionnairesApi(client).document(id = "c1a0eb87a5384aacba9b2c3fdf9ea19f",version = 2)
+        Group.update_forward_refs()
+        q = QuestionnaireDocument.parse_obj(q_doc)
+        flat_list = get_properties(q)
+        if flat_list[variablename].obj_type == 'MultyOptionsQuestion':
+            result = df.iloc[:0].copy()
+            count =0
+            for i in df[variablename]:
+                if i is not None:
+                    if option_value in i:
+                        result.loc[len(result.index)]=df.iloc[count,:]
+                count = count+1
+            present_columns = result.columns.to_list()
+            json_records = result.reset_index().to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            result = result.rename(columns=question_df.set_index('variable_name')['question_text'])
+            present_columns1 = result.columns.to_list()
+            context = {'data': data,'present_columns':present_columns,'option_value':option_value,'variablename':variablename,'present_columns1':present_columns1}
+            return render(request,'SurveyResult/responselist.html',context)
+        else:
+            result = df[df[variablename]==option_value]
+            present_columns = result.columns.to_list()
+            json_records = result.reset_index().to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            result = result.rename(columns=question_df.set_index('variable_name')['question_text'])
+            present_columns1 = result.columns.to_list()
+            context = {'data': data,'present_columns':present_columns,'option_value':option_value,'variablename':variablename,'present_columns1':present_columns1}
+            return render(request,'SurveyResult/responselist.html',context)
         
+
